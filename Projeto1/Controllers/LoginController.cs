b84898b9546bd184ff415bdae2308a8cc6405ef1
Gabriel <gabrielcorolleite@gmail.com>
2025-03-20 14:55:30 -0300

    Criando envio de dados Login

--- Projeto1/Controllers/LoginController.cs
+++ Projeto1/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Projeto1.Repositorio;
 
 namespace Projeto1.Controllers
 {
@@ -8,5 +10,22 @@ namespace Projeto1.Controllers
         {
             return View();
         }
-    }
-}
+
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositoirio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha == senha)
+            {
+                //Autenticação Bem-Sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha invalidos.";
+            return View();
+
+            }
+        }   
+
+ }
+
