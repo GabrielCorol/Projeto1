15d2227ac8328a8ef27182743590eef1f36a0bf8
Gabriel <gabrielcorolleite@gmail.com>
2025-03-20 15:01:39 -0300

    Criando envio de dados cadastro

---
+++ Projeto1/Controllers/Cadastro.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Models;
+
+namespace Projeto1.Controllers
+{
+    public class Cadastro : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Logn");
+            }
+            return View(usuario);
+        }
+    }
+}
